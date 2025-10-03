9430d930404192519986f2c32dccfef0711fae7e
Michael Galanakis <michael.galanakis@omicron.dk>
2025-10-03 13:53:28 +0200

    adds cross_join

--- R/data_processing.R
+++ R/data_processing.R
@@ -4,3 +4,11 @@ dataset_filter_columns = \(dataset, regex, invert=FALSE) {
   new_columns = names(dataset)[column_selection]
   return(dataset[, ..new_columns])
 }
+
+#' @export
+cross_join = \(x, ...) {
+  y = data.table(...)
+  x[, TEMPORARY.BY := 1]
+  y[, TEMPORARY.BY := 1]
+  merge(x,y, by='TEMPORARY.BY', allow.cartesian = TRUE)[, -'TEMPORARY.BY']
+}
